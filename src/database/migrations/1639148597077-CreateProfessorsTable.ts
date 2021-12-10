cee6e83f214d8ebdb720b98c4f5f71058ec7c406
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2021-12-10 12:18:58 -0300

    changing id type from uuid to varchar

--- src/database/migrations/1639148597077-CreateProfessorsTable.ts
+++ src/database/migrations/1639148597077-CreateProfessorsTable.ts
@@ -7,9 +7,9 @@ export class CreateProfessorsTable1639148597077 implements MigrationInterface {
             new Table({
                 name: 'professors',
                 columns: [
-                    {name: 'id', type: 'uuid', isPrimary: true},
+                    {name: 'id', type: 'varchar', isPrimary: true},
                     {name: 'name', type: 'varchar', length:'80', isUnique: false, isNullable: false},
-                    {name: 'subject', type: 'uuid', isNullable: false}
+                    {name: 'subject', type: 'varchar', isNullable: false}
                 ]
             })
         );
