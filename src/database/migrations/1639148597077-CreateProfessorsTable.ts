3d1997f965e2502d4f43ff2510d0d244581ba6af
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2021-12-10 12:11:00 -0300

    create professor table migration

---
+++ src/database/migrations/1639148597077-CreateProfessorsTable.ts
@@ -0,0 +1,22 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateProfessorsTable1639148597077 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'professors',
+                columns: [
+                    {name: 'id', type: 'uuid', isPrimary: true},
+                    {name: 'name', type: 'varchar', length:'80', isUnique: false, isNullable: false},
+                    {name: 'subject', type: 'uuid', isNullable: false}
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('professors');
+    }
+
+}
